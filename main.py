c1ded787b5c6c1a95036409c868dd754b822857c
Moustafa-Bendari <Moustafa.Bendari23@gmail.com>
2025-01-19 18:55:25 -0700

    commit 2, now there is working code

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("Hello World Test git");
+import random
+
+def guess_age():
+    print("Welcome! I'm going to try to guess your age.")
+    name = input("What's your name? ")
+
+    low, high = 15, 30
+    attempts = 5
+    while attempts > 0:
+        guess = random.randint(low, high)
+        response = input(f"{name}, are you {guess} years old?: ").lower()
+
+        if response == 'y':
+            print(f"Hooray! {name} is {guess} years old!")
+            break
+        else:
+            print("Rats.")
+            hint = input("Am I too (high/low)? ").lower()
+            if hint == "high":
+                high = guess - 1
+            elif hint == "low":
+                low = guess + 1
+        attempts -= 1
+    else:
+        print(f"Sorry, {name}, I couldn't guess your age!")
+
+guess_age()
